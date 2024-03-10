ff0fe3dd6df93fa5a875cab4153d5c53bbe0a4dd
Nakyujin <yujin.nakayama02@gmail.com>
2024-03-10 11:37:07 +0900

    for deployment

--- db.js
+++ db.js
@@ -1,21 +1,16 @@
 
-import mysql from "mysql"
+import mysql from "mysql";
 
+const dbConfig = {
+    host: "142ea3db60a8de72c9bad5104fe37c55",
+    user: "5db2e2c143966a09398cd0f75e3532c0",
+    password: "253414796f98b92ecd92330bd125c596",
+    database: "577ea0df49e53d99dedf75fcc8645753"
+};
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"Pokemonlake212",
-    database:"Blogdb"
-})
+const pool = mysql.createPool(dbConfig);
 
-const pool = mysql.createPool({
-    connectionLimit: 10, 
-    host: 'localhost',
-    user: 'root',
-    password: 'Pokemonlake212',
-    database: 'Blogdb'
-  });
+export const db = pool;
 
   pool.getConnection((err, connection) => {
     if (err) {
