5957482a29d12ffbc19a5c9691d2a182458db187
Nakyujin <yujin.nakayama02@gmail.com>
2024-03-11 00:04:48 +0900

    increased timeout duration

--- db.js
+++ db.js
@@ -3,34 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 10, 
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    acquireTimeout: 60000 // Timeout set to 60 seconds
 });
 
 pool.getConnection((err, connection) => {
     if (err) {
         throw err;
     }
-
+  
     connection.query(`ALTER USER '${process.env.DB_USER}'@'${process.env.DB_HOST}' IDENTIFIED WITH mysql_native_password BY '${process.env.DB_PASSWORD}'`, (err, result) => {
         if (err) {
             throw err;
         }
         console.log('User altered successfully');
-
+  
         connection.release();
-
+  
         pool.end((err) => {
             if (err) {
                 throw err;
